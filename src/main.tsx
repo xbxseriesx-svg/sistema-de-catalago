ad77693ad3a0d48019fb3332404a5e328930fb95
salario-minimo <paulohenriquepmelo@gmail.com>
2026-08-12 09:23:41 -0300

    style: load editor improvement styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
+import './improvements.css'
 import './version-inspect.css'
 import './admin/auth.css'
 
