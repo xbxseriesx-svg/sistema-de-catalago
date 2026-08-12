bc34a83e89d392000c29af31d716a9be1b954cb4
salario-minimo <paulohenriquepmelo@gmail.com>
2026-08-11 21:38:54 -0300

    style: load protected admin authentication styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './version-inspect.css'
+import './admin/auth.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
