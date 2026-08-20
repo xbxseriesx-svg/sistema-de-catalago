c5f2cffa82d1e500fd386e698bb701240ca73b67
xbxseriesx-svg <xbxseriesx@gmail.com>
2026-08-20 10:37:48 -0300

    chore: remover loader extra do carrossel

--- public/template-carousel-mode-v1.js
+++
@@ -1,7 +0,0 @@
-(() => {
-  'use strict';
-  const root = document.documentElement;
-  const editor = location.pathname === '/admin' || location.pathname.startsWith('/admin/');
-  root.classList.toggle('asteryon-editor-surface', editor);
-  root.classList.toggle('asteryon-public-surface', !editor);
-})();
