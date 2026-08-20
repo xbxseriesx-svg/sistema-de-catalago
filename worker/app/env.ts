b0b018c67ef6a772f0559c124fc832dfc7813b7f
xbxseriesx-svg <xbxseriesx@gmail.com>
2026-08-20 19:41:34 -0300

    refactor: iniciar backend Enterprise sem sufixos de versão

---
+++ worker/app/env.ts
@@ -0,0 +1,15 @@
+export type Env = {
+  ASSETS: Fetcher;
+  SUPABASE_URL: string;
+  SUPABASE_PUBLISHABLE_KEY: string;
+  SUPABASE_SECRET_KEY?: string;
+  SUPABASE_SERVICE_ROLE_KEY?: string;
+  REMOTE_IMAGE_HMAC_SECRET?: string;
+  SDM_BOOTSTRAP_TOKEN?: string;
+  GOOGLE_CSE_API_KEY?: string;
+  GOOGLE_CSE_CX?: string;
+};
+
+export const COMPANY_ID = 'cmp_asteryon';
+export const ACCESS_COOKIE = '__Host-asteryon_access';
+export const REFRESH_COOKIE = '__Host-asteryon_refresh';
